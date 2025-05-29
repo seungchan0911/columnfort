d95c8bc1d455bf796f2554a3cee2e11af12534b3
seungchan0911 <167081641+seungchan0911@users.noreply.github.com>
2025-05-29 16:51:58 +0900

    Add files via upload

--- js/sign_in.js
+++ js/sign_in.js
@@ -7,23 +7,23 @@ changeInput.addEventListener("change", () => {
     setTimeout(() => {
         if (changeInput.checked) {
             form.innerHTML = `
-                <div class="form-start">
-                    <h1>Sign in</h1>
-                    <div class="input-group">
-                        <input type="text" name="id" placeholder="id" autocomplete="off">
-                        <input type="password" name="password" placeholder="password" autocomplete="off">
-                    </div>
+            <div class="form-start">
+                <h1>Sign in</h1>
+                <div class="input-group">
+                    <input type="text" name="id" placeholder="id" autocomplete="off">
+                    <input type="password" name="password" placeholder="password" autocomplete="off">
                 </div>
-                <div class="form-end">
-                    <button>Sign in</button>
-                    <label for="change">Don't have an account?</label>
-                </div>
+                <button type="submit">Sign in</button>
+            </div>
+            <div class="form-end">
+                <label for="change">Don't have an account?</label>
+            </div>
             `
         } else {
             form.innerHTML = `
 
                 <div class="form-start">
-                    <h1>Create Account</h1>
+                    <h1>Create account</h1>
                     <div class="input-group">
                         <input type="text" name="username" placeholder="username" autocomplete="off">
                         <input type="text" name="id" placeholder="id" autocomplete="off">
@@ -31,9 +31,9 @@ changeInput.addEventListener("change", () => {
                         <input type="text" name="phone_number" placeholder="phone_number" autocomplete="off">
                         <input type="text" name="email" placeholder="email" autocomplete="off">
                     </div>
+                    <button type="submit">Sign up</button>
                 </div>
                 <div class="form-end">
-                    <button>Sign up</button>
                     <label for="change">Are you ready for Sign in?</label>
                 </div>
             `
