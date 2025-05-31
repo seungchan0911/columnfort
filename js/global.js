2abe6f412af3b3912a8b3d2f573ed715306651cf
seungchan0911 <167081641+seungchan0911@users.noreply.github.com>
2025-05-31 10:15:06 +0900

    Add files via upload

--- js/global.js
+++ js/global.js
@@ -1,7 +1,7 @@
-function search() {
-    const searchBtn = document.querySelector(".search-btn")
-    const searchInput = document.querySelector(".search-input")
-    const searchToggle = document.querySelector(".search-toggle")
+function Search() {
+    const searchBtn = document.querySelector("header .search-btn")
+    const searchInput = document.querySelector("header .search-input")
+    const searchToggle = document.querySelector("header .search-toggle")
 
     searchToggle.onclick = () => {
         searchInput.focus()
@@ -19,4 +19,22 @@ function search() {
         })
 }
 
-search()
+Search()
+
+function mobileSearch() {
+    const searchBtn = document.querySelector(".mobile-menu .search-btn")
+    const searchInput = document.querySelector(".mobile-menu .search-input")
+
+    searchBtn.onclick = () => {
+            if (searchInput.value.trim())
+                window.location.href = `../collection.html?id=${searchInput.value}`
+        }
+        
+        searchInput.addEventListener("keydown", (e) => {
+            if (e.key === "Enter" && searchInput.value.trim()) {
+                window.location.href = `../collection.html?id=${searchInput.value}`
+            }
+        })
+}
+
+mobileSearch()
